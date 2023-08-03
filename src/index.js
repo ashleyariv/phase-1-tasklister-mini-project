2660d862540690a7044b7ef9e0d32409d708851c
Ashley Rivera <ashleyariv@gmail.com>
2023-08-03 12:00:24 -0400

    task list porject

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newTaskForm = document.getElementById('create-task-form');
+  
+  newTaskForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const taskText = event.target['new-task-description'].value;
+    const taskList = document.getElementById('tasks');
+    const taskItem = document.createElement('li');
+    taskItem.textContent = taskText;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = ' X';
+    deleteButton.addEventListener('click', () => {
+      taskItem.remove();
+    })
+    taskItem.appendChild(deleteButton);
+
+    taskList.appendChild(taskItem);
+    event.target.reset();
+  })
 });
